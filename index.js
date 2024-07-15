ab9177dfc4452be02ce71d7972e5ef372113b7bc
TessRuvimbo <gomberatess@gmail.com>
2024-07-15 16:13:14 +0200

    added functions to change city and temperature

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature =  response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;}
+
+function searchCity(city){
+    let apiKey= "d324164f4b20a2b7oa3417dta85e424d";
+    let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);}
+
+
+
+
+
 function handleSearchSubmit(event) {event.preventDefault();
 let searchInput = document.querySelector("#input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;}
+
+searchCity(searchInput.value);}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 
