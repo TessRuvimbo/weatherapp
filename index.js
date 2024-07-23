45701c03a3bf2fdce9a816b132f29e48e090b7f8
TessRuvimbo <gomberatess@gmail.com>
2024-07-23 16:23:12 +0200

    updated weather forecast and formatted icons.

--- index.js
+++ index.js
@@ -46,28 +46,34 @@ let searchInput = document.querySelector("#input");
 
 searchCity(searchInput.value);}
 
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
    let apiKey ="d324164f4b20a2b7oa3417dta85e424d";
    let apiUrl =`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-axios(apiUrl).then(displayForecast);
+axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast() {
-   let days=["Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response) {
    let forecastHTML = "";
 
-   days.forEach(function (day){
-
+   response.data.daily.forEach(function (day, index){
+    if (index < 5) {
     forecastHTML = forecastHTML + `
    <div class= "weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌧️</div>
-    <div class="weather-forecast-temps">
-    <div class="temp">
-        <strong>17°</strong></div>
-    <div class="temp">8°</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}°</strong></div>
+    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
     </div>
-</div>`;
+</div>`;}
    });
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHTML;}
@@ -75,6 +81,7 @@ forecastElement.innerHTML = forecastHTML;}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Paris");
 
 
 
